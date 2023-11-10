65594312de028f1d0d36a314323a2b52d7e8aeb1
abhi7773 <146977343+abhi7773@users.noreply.github.com>
2023-11-10 16:52:03 +0530

    Update loljs.js

--- loljs.js
+++ loljs.js
@@ -1,34 +1,9 @@
-let searchform = document.querySelector('.search-form');
- 
-document.querySelector('#search-btn').onclick = () =>
-{
-    searchform.classList.toggle('active');
-    loginform.classList.remove('active');
-    navbar.classList.remove('active');
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
 }
 
-let loginform = document.querySelector('.login-form');
- 
-document.querySelector('#login-btn').onclick = () =>
-{
-    loginform.classList.toggle('active');
-    searchform.classList.remove('active');
-    navbar.classList.remove('active');
-}
-
-let navbar = document.querySelector('.navbar');
- 
-document.querySelector('#menu-btn').onclick = () =>
-{
-    navbar.classList.toggle('active');
-    searchform.classList.remove('active');
-    loginform.classList.remove('active');
-}
-
-
-window.onscroll = () =>
-{
-    searchform.classList.remove('active');
-    loginform.classList.remove('active');
-    navbar.classList.remove('active');
+function closemenu() {
+    sidemenu.style.right = "-200px";
 }
